5efcbc5d8759512744ba9a07933fee55019218ab
Ignacio60DG <idiegar2502@g.educaand.es>
2024-10-09 08:49:06 +0200

    Capicúa

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -8,14 +9,42 @@ public class Main {
 
         Scanner sc = new Scanner (System.in);
 
+
+        System.out.println("Escribe la cifra:");
+        int cifra1 = sc.nextInt();
+        System.out.println("Escribe la siguiente cifra:");
+        int cifra2 = sc.nextInt();
+        System.out.println("Escribe la siguiente cifra:");
+        int cifra3 = sc.nextInt();
+        System.out.println("Escribe la siguiente cifra:");
+        int cifra4 = sc.nextInt();
+        /*
         String numero = sc.nextLine();
 
         while(numero.length() > 4) {
             numero = sc.next();
         }
-
+        */
         sc.close();
 
+
+
+        if(cifra1 == 0 && cifra2 == 0 && cifra3 == 0 && cifra4 == 0) {
+            System.out.println("Es cero");
+        }else if(cifra1 == 0 && cifra2 == 0 && cifra3 == 0 && cifra4<=9 && cifra3>0) {
+            System.out.println("Es de una cifra por lo que es capicúa");
+        } if (cifra1 == 0 && cifra2 == 0 && cifra3<=9 && cifra3>0 && cifra3==cifra4) {
+            System.out.println("Es capicúa");
+        }else if (cifra1 == 0 && cifra2<=9 && cifra2>0 && cifra2==cifra4 && cifra3<=9 && cifra3>0) {
+            System.out.println("Es capicúa");
+        }else if (cifra1<=9 && cifra1>0 && cifra1==cifra4 && cifra2==cifra3 && cifra2<=9 && cifra2>0 && cifra3<=9 && cifra3>0 && cifra4<=9 && cifra4>0) {
+            System.out.println("Es capicúa");
+        }else{
+            System.out.println("No es capicúa o no has puesto de cifra en cifra");
+        }
+    }
+
+        /*
         if(numero.length() < 2){
             System.out.println("es capicuo");
         } else if (numero.length() < 3) {
@@ -33,5 +62,8 @@ public class Main {
         }else{
                 System.out.println("No es un numero entre 0 y 9999");
         }
+
     }
+
+         */
 }
